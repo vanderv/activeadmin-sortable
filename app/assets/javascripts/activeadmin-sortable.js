ba7e0a8d74bc30bc60180ea5fdf5a1c8dcfc2430
Ivan Nesterovich <ivan.nesterovich@digitalizm.com>
2014-01-07 19:50:21 +0300

    Add support jquery-ui-rails Add support rails 4 Add sortable between page.

--- app/assets/javascripts/activeadmin-sortable.js
+++ app/assets/javascripts/activeadmin-sortable.js
@@ -1,4 +1,6 @@
-//= require jquery-ui
+//= require jquery.ui.sortable
+//= require jquery.ui.droppable
+//= require jquery.ui.effect-highlight
 
 (function($) {
   $(document).ready(function() {
@@ -6,19 +8,57 @@
   });
 
   $.fn.activeAdminSortable = function() {
+
     this.sortable({
+      start: function(event, ui) {
+        not_sortable = ui.item.parent().find("tr").not(ui.helper, ui.placeholder);
+        $('td', not_sortable).each(function(n){
+          $("td", ui.helper).eq(n).css('width', $(this).width());
+        });
+      },
+      stop: function(event, ui) {
+        $('td', ui.item).each(function(n){
+          $(this).css('width', "auto");
+        });
+      },      
       update: function(event, ui) {
         var url = ui.item.find('[data-sort-url]').data('sort-url');
-
+        var new_position = ui.item.index() + 1;
         $.ajax({
           url: url,
           type: 'post',
-          data: { position: ui.item.index() + 1 },
-          success: function() { window.location.reload() }
+          data: { position: new_position },
+          success: function() {
+            ui.item.children('td').effect('highlight', {}, 1000)
+          }
         });
+
       }
     });
 
+    $(".pagination .page:not(.current)").droppable({
+      activeClass: "drop_active",
+      hoverClass: "drop_hover",
+      tolerance: "pointer",
+      drop: function(event, ui){
+        var url = ui.draggable.find('[data-sort-url]').data('sort-url');
+        var quantity_items = ui.draggable.parent().find("tr").size() - 1;
+        var new_position = quantity_items * parseInt($(event.target).text()) - 1; 
+        $.ajax({
+          url: url,
+          type: 'post',
+          data: { position: new_position },
+          success: function() { 
+            url = $(event.target).find("a").attr("href");
+            window.location.href = url;
+          }
+        });
+
+      }   
+    });
+
+
     this.disableSelection();
   }
+  
 })(jQuery);
